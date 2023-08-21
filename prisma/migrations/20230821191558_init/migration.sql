0e00e7e303ba38c326f37a1b55ee55179c32a52a
Paweł Kurek <pawel.kurek975@gmail.com>
2023-08-21 21:27:50 +0200

    prisma initial setup

---
+++ prisma/migrations/20230821191558_init/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "firstName" TEXT NOT NULL,
+    "lastName" TEXT NOT NULL,
+    "gender" TEXT NOT NULL,
+    "photo" TEXT NOT NULL
+);
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,27 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import { PrismaClient } from "@prisma/client";
 
-export default function Home() {
+// It's based on pages router, I'm using App router, go to nextJS docs to App router fetching
+const prisma = new PrismaClient();
+
+export async function getServerSideProps() {
+  const users = await prisma.user.findMany();
+  return {
+    props: {
+      initialUsers: users,
+    },
+  };
+}
+
+export default function Home({ initialUsers }: any) {
+  console.log(initialUsers);
   return (
     <section>
       <header>
         <h1>Home page content</h1>
+        <h2>Users</h2>
+        {initialUsers}
       </header>
     </section>
   );
